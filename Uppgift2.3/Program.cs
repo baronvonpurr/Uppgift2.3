a97d32dd919c11a184a865718b4827c08cf5973a
baronvonpurr <robinhood.svensson@hotmail.com>
2023-09-13 10:52:35 +0200

    Add project files.

---
+++ Uppgift2.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många meter kan du hoppa");
+            double längd = double.Parse(Console.ReadLine());
+            double skillnad = 2.45 - längd;
+            Console.WriteLine("Det är " + skillnad + "meter kortare än rekordet på 2,45 meter!");
+        }
+    }
+}
